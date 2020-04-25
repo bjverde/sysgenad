0f6a9412e5c479d6be04c47fb08d18ef8b8714bd
bjverde <bjverde@yahoo.com.br>
2020-04-25 12:18:31 -0300

    :hammer: #1 melhorando a tela

--- app/control/forms/Gen00.class.php
+++ app/control/forms/Gen00.class.php
@@ -16,11 +16,21 @@ class Gen00 extends TPage
             
             $pagestep = GenStepHelper::getStepPage(GenStepHelper::STEP00);
             
+            $formDin = new TFormDin('Configurações do PHP e informações iniciais');
+            //$formDin->addSelectField('DBMS', 'Escolha o tipo de Banco de Dados:', true, $dbType, null, null, null, null, null, null, ' ', 0);
+            $dbType = FormDinHelper::getListDBMS();
+            $formDin->addSelectField('DBMS', 'Escolha o tipo de Banco de Dados:', true, $dbType);
+            $formDin->addTextField('GEN_SYSTEM_ACRONYM','Sigla do Sistema', 50, true);
+            $formDin->addTextField('GEN_SYSTEM_NAME', 'Nome do sistem', 50, true);
+
+            $this->form = $formDin->getAdiantiObj();
+
             // wrap the page content using vertical box
             $vbox = new TVBox;
             $vbox->style = 'width: 100%';
             $vbox->add( $pagestep );
             $vbox->add( $this->html );
+            $vbox->add( $this->form );
             parent::add($vbox);
         }
         catch (Exception $e)
