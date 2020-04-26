d10395750528af4ac9ea02fb6d93df0964448adb
bjverde <bjverde@yahoo.com.br>
2020-04-25 21:18:35 -0300

    :memo: melhorando documentação

--- app/control/FormDin5/webform/TFormDin.class.php
+++ app/control/FormDin5/webform/TFormDin.class.php
@@ -43,14 +43,14 @@
 /**
  * Classe para criação de formulários web para entrada de dados
  * ------------------------------------------------------------------------
- * Esse é o FormDin 5, que é uma reconstrução do FormDin 4 Sobre o Adianti 7.1
+ * Esse é o FormDin 5, que é uma reconstrução do FormDin 4 Sobre o Adianti 7.X
+ * os parâmetros do metodos foram marcados com:
  * 
- * FormDin 5 - Alguns parametros estão marcados como DEPRECATED por não 
- * funcionar no Adianti foram mantidos para diminuir o impacto sobre
- * as migrações.
- *
- * FORMDIN5 = Parametro novo disponivel apenas na nova versão
- * NOT_IMPLEMENTED = parametros que serão implementados em versões futuras
+ * NOT_IMPLEMENTED = Parâmetro não implementados, talvez funcione em 
+ *                   verões futuras do FormDin. Não vai fazer nada
+ * DEPRECATED = Parâmetro que não vai funcionar no Adianti e foi mantido
+ *              para o impacto sobre as migrações. Vai gerar um Warning
+ * FORMDIN5 = Parâmetro novo disponivel apenas na nova versão
  * ------------------------------------------------------------------------
  * 
  * @author Reinaldo A. Barrêto Junior
@@ -63,14 +63,8 @@ class TFormDin
      * Método construtor da classe do Formulario Padronizado em BoorStrap
      * ------------------------------------------------------------------------
      * Esse é o FormDin 5, que é uma reconstrução do FormDin 4 Sobre o Adianti 7.X
-     * os parâmetros do metodos foram marcados com:
-     * 
-     * NOT_IMPLEMENTED = Parâmetro não implementados, talvez funcione em 
-     *                   verões futuras do FormDin. Não vai fazer nada
-     * DEPRECATED = Parâmetro que não vai funcionar no Adianti e foi mantido
-     *              para o impacto sobre as migrações. Vai gerar um Warning
-     * FORMDIN5 = Parâmetro novo disponivel apenas na nova versão
-
+     * os parâmetros do metodos foram marcados veja documentação da classe para
+     * saber o que cada marca singinifica.
      * ------------------------------------------------------------------------
      * <code>
      * 	$frm = new TFormDin('Título do Formuláio');
@@ -142,10 +136,11 @@ class TFormDin
 
     /**
     * Adiciona um campo oculto ao layout
-    * 
-    * FormDin 5 - Alguns parametros foram marcados com NOT_IMPLEMENTED
-    * por não funcionar no Adianti 7.X e foram mantidos
-    * para diminuir o impacto sobre a migração
+    * ------------------------------------------------------------------------
+    * Esse é o FormDin 5, que é uma reconstrução do FormDin 4 Sobre o Adianti 7.X
+    * os parâmetros do metodos foram marcados veja documentação da classe para
+    * saber o que cada marca singinifica.
+    * ------------------------------------------------------------------------
     *
     * @param string $strName       - 1: Id do Campo
     * @param string $strValue      - 2: Valor inicial
@@ -164,22 +159,23 @@ class TFormDin
 
     /**
      * Adicionar campo entrada de dados texto livre.
-     * 
-     * FormDin 5 - Alguns parametros marcados com NOT_IMPLEMENTED
-     * por não funcionar no Adianti 7.X e foram mantidos
-     * para diminuir o impacto sobre a migração
+     * ------------------------------------------------------------------------
+     * Esse é o FormDin 5, que é uma reconstrução do FormDin 4 Sobre o Adianti 7.X
+     * os parâmetros do metodos foram marcados veja documentação da classe para
+     * saber o que cada marca singinifica.
+     * ------------------------------------------------------------------------
      *
      * @param string $id              -  1: ID do campo
      * @param string $strLabel        -  2: Label do campo
      * @param integer $intMaxLength   -  3: tamanho máximo de caracteres
      * @param boolean $boolRequired   -  4: Obrigatorio ou não. DEFAULT = False.
-     * @param integer $intSize        -  5: DESATIVADO quantidade de caracteres visíveis
+     * @param integer $intSize        -  5: NOT_IMPLEMENTED quantidade de caracteres visíveis
      * @param string $strValue        -  6: texto preenchido
-     * @param boolean $boolNewLine    -  7: DESATIVADO Nova linha
-     * @param string $strHint         -  9: DESATIVADO
+     * @param boolean $boolNewLine    -  7: NOT_IMPLEMENTED Nova linha
+     * @param string $strHint         -  9: NOT_IMPLEMENTED
      * @param string $strExampleText  -  9: PlaceHolder é um Texto de exemplo
-     * @param boolean $boolLabelAbove - 10: DESATIVADO - Label sobre
-     * @param boolean $boolNoWrapLabel- 11: DESATIVADO
+     * @param boolean $boolLabelAbove - 10: NOT_IMPLEMENTED - Label sobre
+     * @param boolean $boolNoWrapLabel- 11: NOT_IMPLEMENTED
      * @return TEntry
      */
     public function addTextField(string $id
@@ -208,10 +204,11 @@ class TFormDin
 
     /**
      * Cria um RadioGroup com efeito visual de Switch dp BootStrap
-     * 
-     * FormDin 5 - Alguns parametros foram DESATIVADO
-     * por não funcionar no Adianti 7.1 e foram mantidos
-     * para diminuir o impacto sobre a migração
+     * ------------------------------------------------------------------------
+     * Esse é o FormDin 5, que é uma reconstrução do FormDin 4 Sobre o Adianti 7.X
+     * os parâmetros do metodos foram marcados veja documentação da classe para
+     * saber o que cada marca singinifica.
+     * ------------------------------------------------------------------------
      * 
      * @param string $id            - 1: ID do campo
      * @param string $strLabel      - 2: Label do campo
@@ -233,10 +230,11 @@ class TFormDin
  
     /**
      * Adicionar campo entrada de dados texto com mascara
-     * 
-     * FormDin 5 - Alguns parametros foram DESATIVADO
-     * por não funcionar no Adianti 7.1 e foram mantidos
-     * para diminuir o impacto sobre a migração
+     * ------------------------------------------------------------------------
+     * Esse é o FormDin 5, que é uma reconstrução do FormDin 4 Sobre o Adianti 7.X
+     * os parâmetros do metodos foram marcados veja documentação da classe para
+     * saber o que cada marca singinifica.
+     * ------------------------------------------------------------------------
      * 
      * S - Represents an alpha character (A-Z,a-z)
      * 9 - Represents a numeric character (0-9)
@@ -246,10 +244,10 @@ class TFormDin
      * @param string $strLabel        - 2: Rotulo do campo que irá aparece na tela
      * @param boolean $boolRequired   - 3: Obrigatorio
      * @param string $strMask         - 4: A mascara
-     * @param boolean $boolNewLine    - 5: DESATIVADO Nova linha
+     * @param boolean $boolNewLine    - 5: NOT_IMPLEMENTED Nova linha
      * @param string $strValue        - 6: texto preenchido
-     * @param boolean $boolLabelAbove - 7: DESATIVADO - Label sobre
-     * @param boolean $boolNoWrapLabel- 8: DESATIVADO
+     * @param boolean $boolLabelAbove - 7: NOT_IMPLEMENTED - Label sobre
+     * @param boolean $boolNoWrapLabel- 8: NOT_IMPLEMENTED
      * @param string $strExampleText  - 9: PlaceHolder é um Texto de exemplo
      * @return void
      */
@@ -263,45 +261,46 @@ class TFormDin
     }    
 
     /**
-    * Adicionar campo tipo combobox ou menu select
-    *
-    * FormDin 5 - Alguns parametros foram DESATIVADO
-    * por não funcionar no Adianti 7.1 e foram mantidos
-    * para diminuir o impacto sobre a migração
-    *
-    * $mixOptions = array no formato "key=>value". No FormDin 5 só permite array PHP
-    * $strKeyColumn = nome da coluna que será utilizada para preencher os valores das opções
-    * $strDisplayColumn = nome da coluna que será utilizada para preencher as opções que serão exibidas para o usuário
-    * $strDataColumns = informações extras do banco de dados que deverão ser adicionadas na tag option do campo select
-    *
-    * <code>
-    * 	// exemplos
-    * 	$frm->addSelectField('tipo','Tipo:',false,'1=Tipo 1,2=Tipo 2');
-    * 	$frm->addSelectField('tipo','Tipo:',false,'tipo');
-    * 	$frm->addSelectField('tipo','Tipo:',false,'select * from tipo order by descricao');
-    * 	$frm->addSelectField('tipo','Tipo:',false,'tipo|descricao like "F%"');
-    *
-    *  //Exemplo espcial - Campo obrigatorio e sem senhum elemento pre selecionado.
-    *  $frm->addSelectField('tipo','Tipo',true,$tiposDocumentos,null,null,null,null,null,null,' ','');
-    * </code>
-    *
-    * @param string  $strName        - 1: ID do campo
-    * @param string  $strLabel       - 2: Label do campo
-    * @param boolean $boolRequired   - 3: Obrigatorio. Default FALSE
-    * @param mixed   $mixOptions     - 4: array dos valores. no formato "key=>value". No FormDin 5 só permite array PHP
-    * @param boolean $boolNewLine    - 5: DESATIVADO Default TRUE = cria nova linha , FALSE = fica depois do campo anterior
-    * @param boolean $boolLabelAbove - 6: DESATIVADO Default FALSE = Label mesma linha, TRUE = Label acima
-    * @param mixed   $mixValue       - 7: DESATIVADO Valor DEFAULT, informe o ID do array
-    * @param boolean $boolMultiSelect- 8: DESATIVADO Default FALSE = SingleSelect, TRUE = MultiSelect
-    * @param integer $intSize             - 9: DESATIVADO Default 1. Num itens que irão aparecer. 
-    * @param integer $intWidth           - 10: DESATIVADO Largura em Pixels
-    * @param string  $strFirstOptionText - 11: DESATIVADO First Key in Display
-    * @param string  $strFirstOptionValue- 12: DESATIVADO Frist Valeu in Display, use value NULL for required. Para o valor DEFAULT informe o ID do $mixOptions e $strFirstOptionText = '' e não pode ser null
-    * @param string  $strKeyColumn       - 13: DESATIVADO
-    * @param string  $strDisplayColumn   - 14: DESATIVADO
-    * @param string  $boolNoWrapLabel    - 15: DESATIVADO
-    * @param string  $strDataColumns     - 16: DESATIVADO
-    * @return TCombo
+     * Adicionar campo tipo combobox ou menu select
+     * ------------------------------------------------------------------------
+     * Esse é o FormDin 5, que é uma reconstrução do FormDin 4 Sobre o Adianti 7.X
+     * os parâmetros do metodos foram marcados veja documentação da classe para
+     * saber o que cada marca singinifica.
+     * ------------------------------------------------------------------------
+     *
+     * $mixOptions = array no formato "key=>value". No FormDin 5 só permite array PHP
+     * $strKeyColumn = nome da coluna que será utilizada para preencher os valores das opções
+     * $strDisplayColumn = nome da coluna que será utilizada para preencher as opções que serão exibidas para o usuário
+     * $strDataColumns = informações extras do banco de dados que deverão ser adicionadas na tag option do campo select
+     *
+     * <code>
+     * 	// exemplos
+     * 	$frm->addSelectField('tipo','Tipo:',false,'1=Tipo 1,2=Tipo 2');
+     * 	$frm->addSelectField('tipo','Tipo:',false,'tipo');
+     * 	$frm->addSelectField('tipo','Tipo:',false,'select * from tipo order by descricao');
+     * 	$frm->addSelectField('tipo','Tipo:',false,'tipo|descricao like "F%"');
+     *
+     *  //Exemplo espcial - Campo obrigatorio e sem senhum elemento pre selecionado.
+     *  $frm->addSelectField('tipo','Tipo',true,$tiposDocumentos,null,null,null,null,null,null,' ','');
+     * </code>
+     *
+     * @param string  $strName        - 1: ID do campo
+     * @param string  $strLabel       - 2: Label do campo
+     * @param boolean $boolRequired   - 3: Obrigatorio. Default FALSE
+     * @param mixed   $mixOptions     - 4: array dos valores. no formato "key=>value". No FormDin 5 só permite array PHP
+     * @param boolean $boolNewLine    - 5: NOT_IMPLEMENTED Default TRUE = cria nova linha , FALSE = fica depois do campo anterior
+     * @param boolean $boolLabelAbove - 6: NOT_IMPLEMENTED Default FALSE = Label mesma linha, TRUE = Label acima
+     * @param mixed   $mixValue       - 7: NOT_IMPLEMENTED Valor DEFAULT, informe o ID do array
+     * @param boolean $boolMultiSelect- 8: NOT_IMPLEMENTED Default FALSE = SingleSelect, TRUE = MultiSelect
+     * @param integer $intSize             - 9: NOT_IMPLEMENTED Default 1. Num itens que irão aparecer. 
+     * @param integer $intWidth           - 10: NOT_IMPLEMENTED Largura em Pixels
+     * @param string  $strFirstOptionText - 11: NOT_IMPLEMENTED First Key in Display
+     * @param string  $strFirstOptionValue- 12: NOT_IMPLEMENTED Frist Valeu in Display, use value NULL for required. Para o valor DEFAULT informe o ID do $mixOptions e $strFirstOptionText = '' e não pode ser null
+     * @param string  $strKeyColumn       - 13: NOT_IMPLEMENTED
+     * @param string  $strDisplayColumn   - 14: NOT_IMPLEMENTED
+     * @param string  $boolNoWrapLabel    - 15: NOT_IMPLEMENTED
+     * @param string  $strDataColumns     - 16: NOT_IMPLEMENTED
+     * @return TCombo
     */     
     public function addSelectField(string $id
                                   ,string $strLabel
