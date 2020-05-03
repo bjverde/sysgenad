e8a8519cc079ec37081c9b18604942a41305bb70
bjverde <bjverde@yahoo.com.br>
2020-05-03 03:05:30 -0300

    :hammer: #1 avisos

--- app/control/forms/Gen00.class.php
+++ app/control/forms/Gen00.class.php
@@ -1,6 +1,10 @@
 <?php
 class Gen00 extends TPage
 {
+
+    // trait com onReload, onSearch, onDelete...
+    use Adianti\Base\AdiantiStandardListTrait;
+
     /**
      * Constructor method
      */
@@ -16,7 +20,15 @@ class Gen00 extends TPage
             
             $pagestep = GenStepHelper::getStepPage(GenStepHelper::STEP00);
             
-            $frm = new TFormDin('Configurações do PHP e informações iniciais');
+            $frm = new TFormDin('Configurações');
+
+            $html = '<h3>Campos:</h3>
+            <ul>
+                <li>Sigla do Sistema: A sigla do sistema é o ID do sistema. Será utiliza para criar a pasta do novo sistema. Por padrão aparecerá no canto superior esquerdo</li>
+                <li>Versão do sistema: É número da versão. É recomendável utilizar <a href="https://semver.org/lang/pt-BR/" target="_blank">o versionamento semântico</a></li>
+                <li>Nome do sistema: É descrição completa do nome do sistema ou o que significa a sigla</li>
+            </ul>';
+            $frm->addHtmlField('aviso',$html,null,null);
             //$formDin->addSelectField('DBMS', 'Escolha o tipo de Banco de Dados:', true, $dbType, null, null, null, null, null, null, ' ', 0);
             $dbType = FormDinHelper::getListDBMS();
             $frm->addSelectField('DBMS', 'Escolha o tipo de Banco de Dados:', true, $dbType);
@@ -25,6 +37,15 @@ class Gen00 extends TPage
             $frm->addTextField('GEN_SYSTEM_NAME', 'Nome do sistem', 50, true);
 
             $this->form = $frm->show();
+
+
+            // add form actions
+            // O Adianti permite a Internacionalização - A função _t('string') serve
+            //para traduzir termos no sistema. Veja ApplicationTranslator escrevendo
+            //primeiro em ingles e depois traduzindo
+            $this->form->addAction(_t('Save'), new TAction(array($this, 'onSave')), 'far:check-circle green');
+            $this->form->addActionLink(_t('Clear'),  new TAction([$this, 'clear']), 'fa:eraser red');
+
 
             // wrap the page content using vertical box
             $vbox = new TVBox;
@@ -40,6 +61,23 @@ class Gen00 extends TPage
         }
     }
     
-    function loadPage()
-    {}
+    /**
+     * Clear filters
+     */
+    public function clear()
+    {
+        $this->clearFilters();
+        $this->onReload();
+    }
+
+    public function onSave($param)
+    {
+        $data = $this->form->getData();
+        $this->form->setData($data);
+
+        //Função do FormDin para Debug
+        FormDinHelper::d($param,'$param');
+        FormDinHelper::debug($data,'$data');
+        FormDinHelper::debug($_REQUEST,'$_REQUEST');
+    }
 }
