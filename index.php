f60ec6c4da2a1ee5211d8dffaebb7701b9f4c418
Bjverde <bjverde@yahoo.com.br>
2020-07-06 02:19:20 -0300

    :bug: #14

--- index.php
+++ index.php
@@ -18,10 +18,10 @@ $css         = TPage::getLoadedCSS();
 $js          = TPage::getLoadedJS();
 $content     = str_replace('{HEAD}', $css.$js, $content);
 
+$content     = str_replace('{head_title}', $ini['general']['application'], $content);
 $content     = str_replace('{system_name}', $ini['system']['system_name'], $content);
 $content     = str_replace('{system_name_sub}', $ini['system']['system_name_sub'], $content);
 $content     = str_replace('{system_version}', $ini['system']['version'], $content);
-$content     = str_replace('{head_title}', $ini['general']['application'], $content);
 $content     = str_replace('{logo-lg}', $ini['system']['logo-lg'], $content);
 $content     = str_replace('{logo-link-class}', $ini['system']['logo-link-class'], $content);
 $content     = str_replace('{login-link}', $ini['system']['login-link'], $content);
