c13cbe2ecfef4c34b1e9151ca94d7c3ee2189a68
bjverde <bjverde@yahoo.com.br>
2020-09-24 01:51:44 -0300

    :hammer: #55 addMethod_onSave

--- app/control/controllers/TCreateForm.class.php
+++ app/control/controllers/TCreateForm.class.php
@@ -679,12 +679,10 @@ class TCreateForm extends TCreateFileContent
         if( $this->getTableType() == TableInfo::TB_TYPE_VIEW ){
             $this->addMethod_onClear($qtdTab);
         }
-        /*
         if ($this->getTableType() == TableInfo::TB_TYPE_TABLE) {
             $this->addMethod_onSave($qtdTab);
         }
         $this->addBlankLine();
-        */
     }        
     //--------------------------------------------------------------------------------------
     public function addButtons($qtdTab)
