2cafc42b4b90b4eaa4676f380d6deadeb4d2e9e6
bjverde <bjverde@yahoo.com.br>
2020-11-10 02:54:43 -0300

    :hammer: #59 melhorando form

--- app/control/controllers/TCreateForm.class.php
+++ app/control/controllers/TCreateForm.class.php
@@ -638,9 +638,8 @@ class TCreateForm extends TCreateFileContent
         $this->addLine($qtdTab.ESP.ESP.'$resultado = $controller->save( $vo );');
         $this->addLine($qtdTab.ESP.ESP.'if( is_int($resultado) && $resultado!=0 ) {');
         $this->addLine($qtdTab.ESP.ESP.ESP.'//$text = TFormDinMessage::messageTransform($text); //Tranform Array in Msg Adianti');
-        $this->addLine($qtdTab.ESP.ESP.ESP.'new TMessage(TFormDinMessage::TYPE_INFO, _t(\'Record saved\') );');            
-        $this->addLine($qtdTab.ESP.ESP.ESP.'$this->frm->addMessage(Message::GENERIC_SAVE);');
-        $this->addLine($qtdTab.ESP.ESP.ESP.'$this->frm->clearFields();');
+        $this->addLine($qtdTab.ESP.ESP.ESP.'$this->frm->addMessage( _t(\'Record saved\') );');
+        $this->addLine($qtdTab.ESP.ESP.ESP.'//$this->frm->clearFields();');
         $this->addLine($qtdTab.ESP.ESP.'}else{');
         $this->addLine($qtdTab.ESP.ESP.ESP.'$this->frm->addMessage($resultado);');
         $this->addLine($qtdTab.ESP.ESP.'}');
