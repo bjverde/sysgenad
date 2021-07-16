07cf6349c0f55fca57793bcc132b0840759e3ff5
bjverde <bjverde@yahoo.com.br>
2021-07-16 18:00:09 -0300

    :abc: #73

--- index.php
+++ index.php
@@ -19,10 +19,10 @@ $js          = TPage::getLoadedJS();
 $content     = str_replace('{HEAD}', $css.$js, $content);
 
 //--- FORMDIN 5 START ---------------------------------------------------------
-$content     = str_replace('{head_title}', $ini['general']['application'], $content);
+$content     = str_replace('{head_title}'     , $ini['general']['application'], $content);
 $content     = str_replace('{formdin_version}', FormDinHelper::version(), $content);
-$content     = str_replace('{system_version}', $ini['system']['version'], $content);
-$content     = str_replace('{system_name}', $ini['system']['system_name'], $content);
+$content     = str_replace('{system_version}' , $ini['system']['version'], $content);
+$content     = str_replace('{system_name}'    , $ini['system']['system_name'], $content);
 $content     = str_replace('{system_name_sub}', $ini['system']['system_name_sub'], $content);
 $content     = str_replace('{logo-mini}', $ini['system']['logo-mini'], $content);
 $content     = str_replace('{logo-lg}', $ini['system']['logo-lg'], $content);
