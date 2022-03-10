fefc2225b295a0b9694b2efd8bb5e628bef8a649
bjverde <bjverde@yahoo.com.br>
2022-03-10 03:22:45 -0300

    :bug: #81

--- app/lib/menu/AdiantiMenuBuilder.php
+++ app/lib/menu/AdiantiMenuBuilder.php
@@ -6,6 +6,8 @@ class AdiantiMenuBuilder
         switch ($theme)
         {
             case 'theme3':
+            case 'theme3_v5':
+            case 'theme_formdinv':
                 ob_start();
                 $xml = new SimpleXMLElement(file_get_contents($file));
                 $menu = new TMenu($xml, null, 1, 'treeview-menu', 'treeview', '');
@@ -14,7 +16,19 @@ class AdiantiMenuBuilder
                 $menu->show();
                 $menu_string = ob_get_clean();
                 return $menu_string;
-                break;
+            break;
+            case 'theme_formdin':
+                ob_start();
+                $xml = new SimpleXMLElement(file_get_contents($file));
+                $menu = new TMenu($xml,null, 1,'dropdown-menu','nav-item dropdown','nav-link dropdown-toggle');
+                $menu->id    = 'main-menu-top';
+                $menu->show();
+                $menu_string = ob_get_clean();
+                
+                $menu_string = str_replace('class="dropdown-menu level-1" id="main-menu-top"', 'class="nav navbar-nav" id="main-menu-top"', $menu_string);
+                //$menu_string = str_replace('<a href="', '<a class="dropdown-menu" href="', $menu_string);
+                return $menu_string;
+            break;
             default:
                 ob_start();
                 $xml = new SimpleXMLElement(file_get_contents($file));
