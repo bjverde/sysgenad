f97787888acb5ed94a598e86472564d2d36276d7
Reinaldo Araujo Barreto Junior <reinaldo.junior@mpdft.mp.br>
2023-05-23 23:41:31 -0300

    :bug: #91 correção

--- app/lib/widget/FormDin5/webform/TFormDinDaoDbms.class.php
+++ app/lib/widget/FormDin5/webform/TFormDinDaoDbms.class.php
@@ -319,9 +319,8 @@ class TFormDinDaoDbms
 	*/
 	public function serializeFields()
 	{
-		if ( $this->getMetadataDir() && $this->getTableName() )
-		{
-		    $filename = $this->getMetadataDir() . $this->getConnDbType() . '-' . $this->getTableName() . '.ser';
+		if ( $this->getMetadataDir() && $this->getTableName() ){
+		    $filename = $this->getMetadataDir().$this->getType().'-'.$this->getTableName().'.ser';
 		    $data = serialize( $this->getFields() );
 		    file_put_contents( $filename, $data );
 		}
