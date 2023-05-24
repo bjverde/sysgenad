d6f024da5a6c767961f1eb668f7964c070c6f110
Reinaldo Araujo Barreto Junior <reinaldo.junior@mpdft.mp.br>
2023-05-24 01:39:56 -0300

    :hammer: PHP 8.2.6 and PHPUnit 10.1.3

--- app/tests/phpunit_coverage.bat
+++ app/tests/phpunit_coverage.bat
@@ -14,8 +14,10 @@ REM C:\wamp64\bin\php\php7.4.33\php.exe C:\wamp64\bin\phpunit\phpunit-9.5.9.phar
 REM ECHO PHP 8.1.13 and PHPUnit 9.5.9 Simples with Coverage
 REM C:\wamp64\bin\php\php8.1.13\php.exe C:\wamp64\bin\phpunit\phpunit-9.5.9.phar --colors=auto --bootstrap C:\wamp64\www\adinatiApp\sysgenad\init.php --whitelist C:\wamp64\www\adiantiApp\sysgenad\app\control\controllers --coverage-html C:\wamp64\www\adiantiApp\sysgenad\phpunit-code-coverage C:\wamp64\www\adinatiApp\sysgenad\app\tests\
 
-REM ECHO PHP 8.2.6 and PHPUnit 9.1.4 Simples
-C:\wamp64\bin\php\php8.2.6\php.exe C:\wamp64\bin\phpunit\phpunit-9.5.9.phar --colors=auto --bootstrap C:\wamp64\www\adinatiApp\sysgenad\init.php C:\wamp64\www\adinatiApp\sysgenad\app\tests\
+ECHO PHP 8.2.6 and PHPUnit 10.1.3 Simples
+C:\wamp64\bin\php\php8.2.6\php.exe C:\wamp64\bin\phpunit\phpunit-10.1.3.phar --colors=auto --bootstrap C:\wamp64\www\adinatiApp\sysgenad\init.php C:\wamp64\www\adinatiApp\sysgenad\app\tests\
 
+REM ECHO PHP 8.2.6 and PHPUnit 10.1.3 Simples with Coverage
+REM C:\wamp64\bin\php\php8.2.6\php.exe C:\wamp64\bin\phpunit\phpunit-10.1.3.phar --colors=auto --bootstrap C:\wamp64\www\adinatiApp\sysgenad\init.php --whitelist C:\wamp64\www\adiantiApp\sysgenad\app\control\controllers --coverage-html C:\wamp64\www\adiantiApp\sysgenad\phpunit-code-coverage C:\wamp64\www\adinatiApp\sysgenad\app\tests\
 
 cd app\tests\
