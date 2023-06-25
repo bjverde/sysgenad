5cc9f3134a7784d96232db4183db483f57c62aaf
Bjverde <bjverde@yahoo.com.br>
2023-06-25 16:55:28 -0300

    :hammer: #100 removendo variaveis não usadas

--- app/control/controllers/TCreateForm.class.php
+++ app/control/controllers/TCreateForm.class.php
@@ -12,23 +12,6 @@
 
 class TCreateForm extends TCreateFormGeneric
 {
-    private $formId;
-    private $formTitle;
-    private $primaryKeyTable;
-    private $tableRef;
-    private $tableRefClass;
-    private $tableRefClassForm;
-    private $tableRefDAO;
-    private $tableRefVO;
-    private $listColumnsName;
-    private $lines;
-    private $gridType;
-    private $listColumnsProperties;
-    private $tableType = null;
-    private $databaseManagementSystem  = null;
-    private $dtView;
-    private $dtDb;
-
     /**
      * Create file FROM form a table info
      * @param string $pathFolder   - folder path to create file
