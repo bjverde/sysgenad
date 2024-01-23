d3f3ebefe0f5c84bdf91c4a0a6c4c5f70a39a92e
Bjverde <bjverde@yahoo.com.br>
2024-01-23 01:07:42 -0300

    🔨#126 Adianti 7.6.0 - update lib/adianti/log

--- lib/adianti/log/TLogger.php
+++ lib/adianti/log/TLogger.php
@@ -4,11 +4,11 @@ namespace Adianti\Log;
 /**
  * Provides an abstract interface to register LOG files
  *
- * @version    7.5
+ * @version    7.6
  * @package    log
  * @author     Pablo Dall'Oglio
  * @copyright  Copyright (c) 2006 Adianti Solutions Ltd. (http://www.adianti.com.br)
- * @license    http://www.adianti.com.br/framework-license
+ * @license    https://adiantiframework.com.br/license
  */
 abstract class TLogger implements AdiantiLoggerInterface
 {
--- lib/adianti/log/TLoggerSTD.php
+++ lib/adianti/log/TLoggerSTD.php
@@ -6,11 +6,11 @@ use Adianti\Log\TLogger;
 /**
  * Register LOG in Standard Output
  *
- * @version    7.5
+ * @version    7.6
  * @package    log
  * @author     Pablo Dall'Oglio
  * @copyright  Copyright (c) 2006 Adianti Solutions Ltd. (http://www.adianti.com.br)
- * @license    http://www.adianti.com.br/framework-license
+ * @license    https://adiantiframework.com.br/license
  */
 class TLoggerSTD extends TLogger
 {
